9cdcd26c07d8c1ba1d810ae2992cf78d072cf315
Thiago de Lima Batista <159543319+Thiagolimabatista@users.noreply.github.com>
2025-03-18 08:52:06 -0300

    feat: commit

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -10,6 +10,9 @@ const config = {
   username: `${process.env.DATABASE_USERNAME}`,
   password: `${process.env.DATABASE_PASSWORD}`,
   database: `${process.env.DATABASE_NAME}`,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   synchronize: false,
