d6d5783ddb9b14625e264ca12ef8dd9998cdf7bd
Thiago de Lima Batista <159543319+Thiagolimabatista@users.noreply.github.com>
2025-03-18 14:39:29 -0300

    feat: ssl

--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -1,24 +1,28 @@
-import { registerAs } from '@nestjs/config';
+import { ConfigObject, registerAs } from '@nestjs/config';
 import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
 
 dotenvConfig({ path: '.env.development' });
 
-const config = {
+const config: ConfigObject = {
   type: 'postgres',
   host: `${process.env.DATABASE_HOST}`,
   username: `${process.env.DATABASE_USERNAME}`,
   password: `${process.env.DATABASE_PASSWORD}`,
   database: `${process.env.DATABASE_NAME}`,
-  ssl: {
-    rejectUnauthorized: false,
-  },
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   synchronize: false,
-  logging: true,
+  logging: `${process.env.DATABASE_QUERY_LOGGING}`,
   schema: 'public',
   uuidExtension: 'uuid-ossp',
+  ssl: process.env.DB_SSL,
+  extra: {
+    softDelete: true,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
 };
 
 export default registerAs('typeorm', () => config);
