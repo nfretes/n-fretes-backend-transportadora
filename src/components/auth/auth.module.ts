54bc9968eeab09bcb1657966a957d50889604b5d
Thiago de Lima Batista <159543319+Thiagolimabatista@users.noreply.github.com>
2025-03-18 12:29:22 -0300

    feat: remove redis

--- src/components/auth/auth.module.ts
+++ src/components/auth/auth.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { RedisService } from 'src/libs/redisClient';
 import { Company } from '@entities/company.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Company])],
   exports: [TypeOrmModule],
   controllers: [AuthController],
-  providers: [AuthService, RedisService],
+  providers: [AuthService],
 })
 export class AuthModule {}
