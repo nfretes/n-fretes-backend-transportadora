a053280e92d52ed780e1794c896f6beff815b3a4
thiagolimamath <thiago.batista@crm.math.group>
2026-01-14 09:35:34 -0300

    feat: deploy de mgs

--- src/components/sqs/sqs.module.ts
+++ src/components/sqs/sqs.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SQSService } from './sqs.service';
+import { SQSConsumerService } from './sqs-consumer.service';
 import { SqsController } from './sqs.controller';
 
 @Module({
   controllers: [SqsController],
-  providers: [SQSService],
+  providers: [SQSService, SQSConsumerService],
   exports: [SQSService],
 })
 export class SqsModule {}
